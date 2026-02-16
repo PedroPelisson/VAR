5e28e2e28c7375b7fdefa822feb65b014f3aba4f
Pedro Henrique Pelisson <pedrohenriquepelisson@gmail.com>
2026-02-16 14:03:24 -0300

    Replace bounding box detection with pose estimation. Switch model to yolov8m-pose. Refine pose tracking.

--- main.py
+++ main.py
@@ -1,24 +1,30 @@
-import cv2
 from ultralytics import YOLO
+import cv2
 
-model = YOLO("yolov8n.pt")
-
-video_path = "lance.mp4"
-cap = cv2.VideoCapture(video_path)
+model = YOLO('yolov8s-pose.pt')
+cap = cv2.VideoCapture('lance2.mp4')
+tracker_config = "bytetrack.yaml" 
 
 while cap.isOpened():
     success, frame = cap.read()
+    if not success: break
 
-    if success:
+    results = model.track(
+        frame, 
+        persist=True, 
+        tracker=tracker_config,
+        conf=0.1,
+        iou=0.8,
+        imgsz=960,
+        verbose=False
+    )
 
-        results = model.track(frame, persist=True, classes=[0], device="cpu")
+    if results[0].boxes.id is not None:
         annotated_frame = results[0].plot()
+        ids = results[0].boxes.id.cpu().numpy()
         cv2.imshow("VAR", annotated_frame)
-
-        if cv2.waitKey(1) == 27:
-            break
     
-    else:
+    if cv2.waitKey(5) == 27:
         break
 
 cap.release()
